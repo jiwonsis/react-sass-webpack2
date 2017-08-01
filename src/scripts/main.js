554ca381d8c4a7c124411c145f109ad813ba8e8c
Scott Moon <scottmoon@flygoose.xyz>
2017-08-01 11:27:33 +0900

    - 깃 업로드 제외 파일 설정 - 더미 프로젝트 제거 - babel 프리셋 1 설정

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import DemoComponent from './DemoComponent';
 
 import '../styles/styles.scss';
 import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 
+class Main extends Component {
+    render(){
+        return (
+            <div>
+                <h2>Hello!</h2>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<DemoComponent/>, document.getElementById('react-container'));
+
+ReactDOM.render(<Main/>, document.querySelector('.main'));
