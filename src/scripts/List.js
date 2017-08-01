2d801a6566818e24f56997e941a259b77f34c505
Scott Moon <scottmoon@flygoose.xyz>
2017-08-01 12:53:33 +0900

    - 칸반보드 로컬 JSON 파싱 - 리엑스 콤포넌트 화면 정의부분 완성

---
+++ src/scripts/List.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Card from './Card'
+
+
+class List extends Component {
+    render() {
+        var cards = this.props.cards.map((card) => {
+            return <Card key = {card.id}
+                         id = {card.id}
+                         title = {card.title}
+                         desciprion = {card.description}
+                         tasks = {card.tasks} />
+        });
+
+        return (
+            <div className="list">
+                <h1>{this.props.title}</h1>
+                {cards}
+            </div>
+        )
+    }
+}
+
+export default List;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
+import KanbanBoard from './KanbanBoard'
+
 import '../styles/styles.scss';
-import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+// import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 
-class Main extends Component {
-    render(){
-        return (
-            <div>
-                <h2>Hello!</h2>
-            </div>
-        )
-    }
-}
+let cardList = [
+    {
+        id: 1,
+        title: "Read the book",
+        description: "I should read the whole book",
+        status: "in-progress",
+        tasks: []
+    },{
+        id: 2,
+        title: "write some code",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },{
+                id: 2,
+                name: "The kanban Example",
+                done: false
+            },{
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+];
 
-
-ReactDOM.render(<Main/>, document.querySelector('.main'));
+ReactDOM.render(<KanbanBoard cards={cardList} />, document.querySelector('#root'));
